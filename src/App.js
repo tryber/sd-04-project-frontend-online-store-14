76b813e8d2bc7bfb0101a2c7b2b6791785e06499
Kaecio <kaecio@yahoo.com.br>
2020-06-18 20:28:27 -0300

    refazendo a pagina de listagem de produto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 
 function App() {
