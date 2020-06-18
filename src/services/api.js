df0fb70c2348c03e79a15ff9353e23349eaf5b6d
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-06-18 17:22:27 -0300

    Criando a function getCategories()

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 export async function getCategories() {
-  // implement here
+  return await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json())
+    .then((data) => data);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
