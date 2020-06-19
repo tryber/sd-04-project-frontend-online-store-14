f7b81d06e882192025f31f632af938ed7e714746
danilossousa <danilosantossousa@gmail.com>
2020-06-19 16:35:19 -0300

    implementar-modulo-acesso-1.2 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((categories) => categories.json())
+    .then((categories) => categories.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((products) => products.json())
+    .then((products) => products.json());
 }
