12c5a18f559c7e58020823e9be392fa559476ef9
Kaecio <kaecio@yahoo.com.br>
2020-06-19 16:47:55 -0300

    testando requisito 3

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
+import Carrinho from '../carrinho.png';
 
 export default class Home extends Component {
   render() {
     return (
       <h1 data-testid="home-initial-message">
         Digite algum termo de pesquisa ou escolha uma categoria.
+        <Link to="cads">
+        <img src={Carrinho} data-testid="shopping-cart-button" />
+        </Link>
       </h1>
     );
   }
