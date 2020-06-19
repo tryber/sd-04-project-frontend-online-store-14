82533a8ee04ca2d966ead6e514a42c5e3bdb8d27
Kaecio <kaecio@yahoo.com.br>
2020-06-19 11:08:28 -0300

    Finalizado 2 requisito

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h1>;
+    return (
+      <h1 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+    );
   }
 }
