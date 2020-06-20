73bf9327007c85a8af28f3b7bc8f73799d5f2985
danilossousa <danilosantossousa@gmail.com>
2020-06-20 19:09:01 -0300

    req5

--- src/pages/componentes/Conteudo.js
+++ src/pages/componentes/Conteudo.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import Categorias from './Categorias';
 
 export default class Conteudo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      termo: '',
+      itens: [],
+    }
+    this.obterItens = this.obterItens.bind(this);
+  }
+
+  obterItens(termo) {
+    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${termo}`)
+      .then((response) =>  itens.json())
+  }
+
   render() {
+    const { termo } = this.state;
     return (
       <div>
+        <input type="text" data-testid="input" value={termo}
+          onChange={(e) => this.setState({ termo: e.target.value })}
+        />
+        <button type="button" onClick={() => this.obterItens(termo)}>Buscar</button>
         <Categorias />
       </div>
     );
