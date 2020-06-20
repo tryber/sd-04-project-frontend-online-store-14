7c571b7ae8091dabae2d178d754d1a32dba4bbab
danilossousa <danilosantossousa@gmail.com>
2020-06-20 16:27:14 -0300

    req5

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((response) => response.json())
-    .then((data) => data);
+    .then((categories) => categories.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
