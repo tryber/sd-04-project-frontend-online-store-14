5f83279fafa2a3cd03defed1a8f9dc679b2f2757
danilossousa <danilosantossousa@gmail.com>
2020-06-20 19:12:25 -0300

    implementaçao de getProductsFromCategoryAndQuery

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((categories) => categories.json());
+    .then((response) => categories.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((products) => products.json());
+    .then((response) => products.json());
 }
