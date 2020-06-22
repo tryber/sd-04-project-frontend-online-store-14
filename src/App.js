d336ce859323c02dd061781ac1e493dd871704cb
danilossousa <danilosantossousa@gmail.com>
2020-06-21 21:28:43 -0300

    atualizaçao master

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import Home from './pages/home';
+import ProdutoDetalhado from './pages/componentes/ProdutoDetalhado';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/produto_detalhado" component={ProdutoDetalhado} />
         <Route exact path="/" component={Home} />
       </Switch>
     </Router>
---
+++ src/pages/componentes/Produto.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Produto extends Component {
+  render() {
+    const { titulo, foto, preco } = this.props;
+    return (
+      <div style={{ border: '1px solid black', borderRadius: '5px' }} data-testid="product">
+        <p>{titulo}</p>
+        <img src={foto} alt="Produto" />
+        <p>{preco}</p>
+        <Link
+          to={{ pathname: '/produto_detalhado', state: { props: this.props } }}
+          data-testid="product-detail-link"
+        >
+          Detalhes
+        </Link>
+      </div>
+    );
+  }
+}
